9213385a74171894e3d4b58e4cf7f21208e8f6f1
Franz <franzj43@gmail.com>
2024-07-23 21:40:55 -0600

    Add ESLint

--- projects/01-twitter-follow-card/src/App.jsx
+++ projects/01-twitter-follow-card/src/App.jsx
@@ -2,14 +2,14 @@ import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard'
 
 export function App () {
-    return (
-        <section className='App'>
-            <TwitterFollowCard userName='midudev' initialIsFollowing>
-                Miguel Angel Duran
-            </TwitterFollowCard>
-            <TwitterFollowCard userName='MuchFranz'>
-                Franz Much
-            </TwitterFollowCard>
-        </section>
-    )
+  return (
+    <section className='App'>
+      <TwitterFollowCard userName='midudev' initialIsFollowing>
+        Miguel Angel Duran
+      </TwitterFollowCard>
+      <TwitterFollowCard userName='MuchFranz'>
+        Franz Much
+      </TwitterFollowCard>
+    </section>
+  )
 }
