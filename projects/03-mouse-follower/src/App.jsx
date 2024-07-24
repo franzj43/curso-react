be1bd32450789c684759b2e321fc38f318f7a11c
Franz <franzj43@gmail.com>
2024-07-23 21:38:26 -0600

    Change cursor

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -4,6 +4,7 @@ const FollowMouse = () => {
   const [enabled, setEnabled] = useState(false)
   const [position, setPosition] = useState({ x: 0, y: 0 })
 
+  // pointer move
   useEffect(() => {
     console.log('effect', { enabled })
 
@@ -22,18 +23,27 @@ const FollowMouse = () => {
     }
   }, [enabled])
 
+  // change body className
+  useEffect(() => {
+    document.body.classList.toggle('no-cursor', enabled)
+
+    return () => {
+      document.body.classList.remove('no-cursor')
+    }
+  }, [enabled])
   return (
     <>
       <div style={{
         position: 'absolute',
-        backgroundColor: '#09f',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
         borderRadius: '50%',
         opacity: 0.8,
         pointerEvents: 'none',
-        left: -20,
-        top: -20,
-        width: 40,
-        height: 40,
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
         transform: `translate(${position.x}px, ${position.y}px)`
       }}
       />
