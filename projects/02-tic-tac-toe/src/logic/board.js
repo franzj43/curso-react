457f171ae7ab04269f42474cc925da1129dc9c5f
Franz <franzj43@gmail.com>
2024-07-23 21:31:53 -0600

    Added ESLint

--- projects/02-tic-tac-toe/src/logic/board.js
+++ projects/02-tic-tac-toe/src/logic/board.js
@@ -1,20 +1,18 @@
-import { WINNER_COMBOS } from "../constants.js"
+import { WINNER_COMBOS } from '../constants.js'
 
 export const checkWinnerFrom = (boardToCheck) => {
-
-    for (const combo of WINNER_COMBOS) {
-      const [a, b, c] = combo
-      if (
-        boardToCheck[a] &&
+  for (const combo of WINNER_COMBOS) {
+    const [a, b, c] = combo
+    if (
+      boardToCheck[a] &&
         boardToCheck[a] === boardToCheck[b] &&
         boardToCheck[a] === boardToCheck[c]
-      ) {
-        return boardToCheck[a]
-      }
+    ) {
+      return boardToCheck[a]
     }
+  }
 
-    return null
-
+  return null
 }
 
 export const checkEndGame = (newBoard) => {
